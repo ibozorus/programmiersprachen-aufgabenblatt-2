eca5666ab80df8e05cf26aae3687b566e3a24749
Ibrahim Eren KORKMAZ <korkmazie@gmail.com>
2026-05-02 21:42:05 +0200

    Aufgabe 2.2 - defined members of Vec2 struct - implemented and executed tests for initialization of Vec2

--- applications/tests.cpp
+++ applications/tests.cpp
@@ -1,5 +1,6 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest.h>
+#include "vec2.hpp"
 
 int factorial(int n)
 {
@@ -16,6 +17,20 @@ TEST_CASE("factorial computes n!")
   CHECK(factorial(1) == 1);
   CHECK(factorial(5) == 120);
   CHECK(factorial(10) == 3628800);
+}
+TEST_CASE("Vec2 default initialization") {
+  buw::Vec2 a;
+  CHECK(0.0f == a.x);
+  CHECK(0.0f == a.y);
+}
+
+TEST_CASE("Vec2 aggregate initialization") {
+  buw::Vec2 b{5.1f, -9.3f};
+  CHECK(5.1f  == doctest::Approx(b.x));
+  CHECK(-9.3f == doctest::Approx(b.y));
+  buw::Vec2 c{10, -71};
+  CHECK(10.0f  == doctest::Approx(c.x));
+  CHECK(-71 == doctest::Approx(c.y));
 }
 
 int main(int argc, char *argv[])
--- framework/vec2.hpp
+++ framework/vec2.hpp
@@ -2,7 +2,10 @@
 #define BUW_VEC2_HPP
 
 namespace buw {
-
+	struct Vec2 {
+		float x = 0.0f;
+		float y = 0.0f;
+	};
 	//write your Vec2 DTO definition and free function declarations for A2.2 - A2.4 here (within the buw namespaces}
 
 }
